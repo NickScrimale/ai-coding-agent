8c2714221e775e4d3d617100ddfd6b154cfbc22b
Gamer <bscrinale@gmail.com>
2025-10-06 10:48:16 -0500

    added --verbose flag

--- ai-coding-agent/main.py
+++ ai-coding-agent/main.py
@@ -1,6 +1,39 @@
-def main():
-    print("Hello from ai-coding-agent!")
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+args = sys.argv[1:]
+
+user_prompt = " ".join(args)
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
+if len(sys.argv) < 2:
+    print("I need a prompt")
+    sys.exit(1)
+verbose_flag = False
+if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+    verbose_flag = True
+prompt = sys.argv[1]
+
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001',
+    contents=messages
+)
+
+print(response.text)
+if verbose_flag:
+    print(f"User prompt: {prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
