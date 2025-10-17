35654da0d821bb4ae54d564a41a1cd33ae1f004b
Gamer <bscrinale@gmail.com>
2025-10-17 11:22:43 -0500

    finished file contents

--- ai-coding-agent/functions/get_file_content.py
+++ ai-coding-agent/functions/get_file_content.py
@@ -1,3 +1,22 @@
+import os
+
+MAX_CHARS = 10000
 
 def get_file_content(working_directory, file_path):
-    abs_working_dir = os.path.baspath
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: "{file_path}" is not in the working dir'
+    if not os.path.isfile(abs_file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    file_content_string = ""
+    try:
+        with open(abs_file_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            if len(file_content_string) >= MAX_CHARS:
+                file_content_string += (
+                    f'[...File "{file_path}" truncated at 10000 characters]'
+                )
+        return file_content_string
+    except Exception as e:
+        return f"Exception reading file {e}"
