efca16f49726930ea5197369ab7c14c7ddb54f77
Gamer <bscrinale@gmail.com>
2025-10-28 07:54:26 -0500

    added stuff

--- ai-coding-agent/functions/write_file.py
+++ ai-coding-agent/functions/write_file.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 def write_file(working_directory, file_path, content):
     abs_working_dir = os.path.abspath(working_directory)
@@ -19,4 +20,20 @@ def write_file(working_directory, file_path, content):
     except Exception as e:
         return f"Failed to write to file: {file_path}, {e}"
 
-
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Overwrites an existing file or writes to a new file if it doesn't exist (and creates required parent dirs safely)",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file to write.",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The content to write to a file as a string",
+            ),
+        },
+    ),
+)
--- ai-coding-agent/main.py
+++ ai-coding-agent/main.py
@@ -3,12 +3,30 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-# from functions.get_files_info import get_files_info
+from functions.get_files_info import schema_get_files_info
+from functions.get_file_content import schema_get_file_content
+from functions.run_python_file import schema_run_python_file
+from functions.write_file import schema_write_file
+from call_function import call_function
+
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+    - Read the contents of a file
+    - Write to a file (create or update)
+    - Run a Python file with optional arguments
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
 
     args = sys.argv[1:]
 
@@ -17,6 +35,19 @@ def main():
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
+
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+            schema_get_file_content,
+            schema_run_python_file,
+            schema_write_file,
+        ]
+    )
+
+    config=types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt
+    )
 
     if len(sys.argv) < 2:
         print("I need a prompt")
@@ -28,14 +59,26 @@ def main():
 
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents=messages
+        contents=messages,
+        config=config,
     )
 
-    print(response.text)
+    if response is None or response.usage_metadata is None:
+        print("response is malformed")
+        return
     if verbose_flag:
         print(f"User prompt: {prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            result = call_function(function_call_part, verbose_flag)
+            print(result)
+    else:
+        print(response.text)
+
 
 
 
